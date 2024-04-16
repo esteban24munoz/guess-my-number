2f5616430335c2a0b628c6705b9182369ac669ff
AsteroidMCats <121195735+AsteroidMCats@users.noreply.github.com>
2024-04-16 17:44:25 -0500

    Final Submission

--- guess-my-number/src/components/GuessForm.js
+++ guess-my-number/src/components/GuessForm.js
@@ -1,66 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../css/components.css';
 import ResultsTable from "./ResultsTable";
 import WinResults from "./WinResults";
 
-const GuessForm = ({setIsPlaying, maxNum, secretNum}) =>{
+const GuessForm = ({ setIsPlaying, maxNum, secretNum }) => {
 
-    let [hasWon, setHasWon] = useState(false);
-    let [currentGuess, setCurrentGuess] = useState("");
-    let [errorMsg, setErrorMsg] = useState("");
-    let [guessList, setGuessList] = useState([]);
+  let [hasWon, setHasWon] = useState(false);
+  let [currentGuess, setCurrentGuess] = useState("");
+  let [errorMsg, setErrorMsg] = useState("");
+  let [guessList, setGuessList] = useState([]);
 
-    function guessSetter(e){
-      return setCurrentGuess(e.target.value);
+  function guessSetter(e) {
+    return setCurrentGuess(e.target.value);
+  }
+
+  function checkGuess(e) {
+    //prevent form default
+    e.preventDefault();
+
+    if (isNaN(currentGuess)) {
+      return setErrorMsg('Enter a valid number');
+    }
+    else {
+      //push currentGuess into array
+      setGuessList([...guessList, currentGuess]);
     }
 
-    function checkGuess(e){
-      //prevent form default
-      e.preventDefault();
+    setErrorMsg('');
+  }
 
-      if(isNaN(currentGuess)){
-        return setErrorMsg('Maximum must be an integer');
-      }
-      else{
-        //push currentGuess into array
-        setGuessList([...guessList, currentGuess]);
-      }
+  return (
+    <div className="GuessForm">
 
-      setErrorMsg('');
-    }
-
-    return(
-      <div className="GuessForm">
-
-        {hasWon ? <WinResults setIsPlaying={setIsPlaying} guessList={guessList}/> 
-        
+      {hasWon ?
+        <>
+          <WinResults setIsPlaying={setIsPlaying} guessList={guessList} />
+          <ol reversed>
+            <ResultsTable guessList={guessList} currentGuess={currentGuess} secretNum={secretNum} setHasWon={setHasWon} />
+          </ol>
+        </>
         :
         <>
           <h1>Guess a number between 1 and {maxNum}</h1>
-            <form>
-                <div className="input-ctn">
-                    <div className="wrapper">
-                        <label for="maximun">Guess: </label>
-                        <input type="text" id="maximun" value={currentGuess} onChange={guessSetter} autoFocus></input>
-                    </div>
-                    <button onClick={checkGuess}>Guess</button>
-                </div>
-                <div className="errorMsg">
-                  <p>
-                    {errorMsg}
-                  </p>
-                </div>
-                <div className="resultsTable">
-                    <ol reversed>
-                      <ResultsTable guessList={guessList} currentGuess={currentGuess} secretNum={secretNum} setHasWon={setHasWon}/>
-                    </ol>
-                </div>
-            </form>
+          <form>
+            <div className="input-ctn">
+              <div className="wrapper">
+                <label for="maximun">Guess: </label>
+                <input type="text" id="maximun" value={currentGuess} onChange={guessSetter} autoFocus></input>
+              </div>
+              <button onClick={checkGuess}>Guess</button>
+            </div>
+            <div className="errorMsg">
+              <p>
+                {errorMsg}
+              </p>
+            </div>
+            <div className="resultsTable">
+              <ol reversed>
+                <ResultsTable guessList={guessList} currentGuess={currentGuess} secretNum={secretNum} setHasWon={setHasWon} />
+              </ol>
+            </div>
+          </form>
         </>
       }
-        </div>
+    </div>
 
-    );
-  };
+  );
+};
 
-  export default GuessForm;
+export default GuessForm;
--- guess-my-number/src/components/WinResults.js
+++ guess-my-number/src/components/WinResults.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import App from "../App";
 
-
 const WinResults = ({setIsPlaying, guessList}) => {
     return(
         <div>
